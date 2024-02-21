3d10c7ee3f2170d62c4aaf71710d4dceb3fc5ec7
Bruno LACROIX <brlacroix@epfc.eu>
2024-02-21 21:18:28 +0100

    feat: version initiale

---
+++ prbd_2324_c02/Model/User.cs
@@ -0,0 +1,8 @@
+﻿using PRBD_Framework;
+
+namespace prbd_2324_c02.Model;
+
+public class User : EntityBase<PridContext>
+{
+    public int UserId { get; set; }
+}
---
+++ prbd_2324_c02/ViewModel/MainViewModel.cs
@@ -0,0 +1,8 @@
+﻿using prbd_2324_c02.Model;
+
+namespace prbd_2324_c02.ViewModel;
+
+public class MainViewModel : PRBD_Framework.ViewModelBase<User, PridContext>
+{
+    public string Title => "prbd_2324_c02";
+}
