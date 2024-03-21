db74450de098133dd4f62fc31fe49cd3ad8d640b
Zakaria Guendouzi <guendouzi2003@gmail.com>
2024-03-21 15:33:33 +0100

    feat(model) : ajout Operations & Repartions

---
+++ prbd_2324_c02/Model/Operations.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prbd_2324_c02.Model
+{
+    public class Operations
+    {
+        public Operations() { }
+        public int OperationsId {  get; set; }
+
+        public string title {  get; set; }
+
+        public virtual Tricount Tricount { get; set; }
+
+        public double Amount {  get; set; }
+        public DateTime DateTime { get; set; } = DateTime.Now;
+
+        [ForeignKey(nameof(initiator))]
+        public virtual User initiator { get; set; }
+    }
+}
---
+++ prbd_2324_c02/Model/Repartitions.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prbd_2324_c02.Model
+{
+    public class Repartitions
+    {
+        public Repartitions() { }
+
+        [Required]
+        public int weight { get; set; }
+
+        [ForeignKey(nameof(user))]
+        public virtual User user { get; set; }
+
+        [ForeignKey(nameof(operations))]
+        public virtual Operations operations { get; set; }
+    }
+
+    /*
+     public class Message : EntityBase<Model> {
+        public int MessageId { get; set; }
+        public DateTime DateTime { get; set; } = DateTime.Now;
+        public string Body { get; set; }
+        public bool IsPrivate { get; set; }
+
+        [Required, ForeignKey(nameof(Author))]
+        public string AuthorPseudo { get; set; }
+
+        [Required]
+        public virtual Member Author { get; set; }
+
+        [Required, ForeignKey(nameof(Recipient))]
+        public string RecipientPseudo { get; set; }
+
+        [Required]
+        public virtual Member Recipient { get; set; }
+     
+     */
+}
