7ee5dddf00e386d57a1817b0799760c4ec9a96cd
Yannick Denis <1509yadenis@student.epfc.eu>
2024-05-08 15:17:30 +0200

    feat(view mytricount):debut de la vue my tricount + vm

--- prbd_2324_c02/ViewModel/MainViewModel.cs
+++ prbd_2324_c02/ViewModel/MainViewModel.cs
@@ -1,8 +1,19 @@
 ﻿using prbd_2324_c02.Model;
+using PRBD_Framework;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
 
 namespace prbd_2324_c02.ViewModel;
 
 public class MainViewModel : PRBD_Framework.ViewModelBase<User, PridContext>
 {
     public string Title => "prbd_2324_c02";
+
+    public ObservableCollection<Tricount> tricounts { get; set; } = new ();
+    public MainViewModel() {
+        OnRefreshData();
+    }
+    protected override void OnRefreshData() {
+        tricounts.RefreshFromModel(Context.Tricounts.Where(t=> t.Creator.UserId == CurrentUser.UserId  ));   
+    }
 }
