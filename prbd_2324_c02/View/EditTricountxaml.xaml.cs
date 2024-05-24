baa4e92e2e2fb1306b56f71cd76e78469824fdb8
Yannick Denis <1509yadenis@student.epfc.eu>
2024-05-24 12:50:56 +0200

    feat(edittricount):debut de vue

---
+++ prbd_2324_c02/View/EditTricountxaml.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using PRBD_Framework;
+
+namespace prbd_2324_c02.View
+{
+    /// <summary>
+    /// Logique d'interaction pour EditTricountxaml.xaml
+    /// </summary>
+    public partial class EditTricountView : UserControlBase
+    {
+        public EditTricountView()
+        {
+            InitializeComponent();
+        }
+        private void CloseTab_Click(object sender, RoutedEventArgs e) {
+            var tabItem = ((sender as Button).Parent as StackPanel).Parent as TabItem;
+            var tabControl = tabItem.Parent as TabControl;
+            tabControl.Items.Remove(tabItem);
+        }
+    }
+}
---
+++ prbd_2324_c02/ViewModel/EditTricountViewModel.cs
@@ -0,0 +1,22 @@
+﻿using prbd_2324_c02.Model;
+using PRBD_Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prbd_2324_c02.ViewModel
+{
+   public  class EditTricountViewModel: ViewModelBase<User, PridContext>
+    {
+        private Tricount curent;
+        private bool mode;
+        public EditTricountViewModel(Tricount curent, bool isEdit) {
+               mode = isEdit;
+               this.curent = curent;
+        }
+     
+
+    }
+}
