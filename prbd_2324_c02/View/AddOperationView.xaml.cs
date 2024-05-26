06d8a0dba9abae3698c649bb25ca7ac891c981d4
Yannick Denis <1509yadenis@student.epfc.eu>
2024-05-26 11:23:37 +0200

    feat(addOperation):suite addOperation

--- prbd_2324_c02/View/AddOperationView.xaml.cs
+++ prbd_2324_c02/View/AddOperationView.xaml.cs
@@ -11,16 +11,20 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using Microsoft.EntityFrameworkCore.Query.Internal;
+using prbd_2324_c02.Model;
+using PRBD_Framework;
 
 namespace prbd_2324_c02.View
 {
     /// <summary>
     /// Logique d'interaction pour AddOperationView.xaml
     /// </summary>
-    public partial class AddOperationView : Window
+    public partial class AddOperationView : DialogViewModelBase<User,PridContext>
     {
-        public AddOperationView() {
+        public AddOperationView(Tricount tricount ,bool isedit , Operations curent) {
             InitializeComponent();
+            
         }
     }
 }
