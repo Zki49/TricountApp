72c85e741dbc7fb4ba18cfe4ff1dc7517e7e7028
Yannick Denis <1509yadenis@student.epfc.eu>
2024-06-05 23:55:16 +0200

    feat(balanceview ) : inversion balance user +(me)

--- prbd_2324_c02/ViewModel/balanceViewModel.cs
+++ prbd_2324_c02/ViewModel/balanceViewModel.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using prbd_2324_c02.Model;
+using PRBD_Framework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,23 +8,33 @@ using System.Threading.Tasks;
 
 namespace prbd_2324_c02.ViewModel
 {
-    class balanceViewModel
+    class balanceViewModel : ViewModelBase<User, PridContext>
     {
         public String username{ get; set; }
-        public double balance{ get; set; }
+        public string balance{ get; set; }
         public double with{ get; set; }
         public String color{ get; set; }
 
-        public balanceViewModel(String UserName , double balance , String color) { 
+        public balanceViewModel(String UserName , double balance , String color) {
+            if (CurrentUser.FullName.Equals(UserName)) {
+                UserName += "(me)  ";
+            }
             this.username = UserName;
-            this.balance = Math.Round(balance,2);
+            this.balance = Math.Round(balance,2).ToString();
             this.color = color;
             if( balance < 0) {
+                this.username= Math.Round(balance, 2).ToString();
+                this.balance = UserName;
+
                 this.with = -balance;
             } else {
                 this.with = balance;
 
             }
+            if( balance ==0) {
+              this.balance = "0";
+            }
+           
         }
 
     }
